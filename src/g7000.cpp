7850ed723105e3fa1a5078722c7559a08a05aaac
Scott Moore <scott.moore@scottmooresoftware.com>
2019-09-23 12:34:14 +0100

    Add example 8048 code and run it

--- src/g7000.cpp
+++ src/g7000.cpp
@@ -1,17 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "mcs48.h"
 
 using namespace std;
 
 int main()
 {
+  MCS48 mcs48;
 
-  vector<string> msg{"Hello", "C++", "World", "from", "VS Code!"};
+  mcs48.writeROM(0, 0b00100111);  // CLR A
+  mcs48.writeROM(1, 0b00110111);  // CPL A
+  mcs48.writeROM(2, 0b00100111);  // CLR A
+  mcs48.writeROM(3, 0b00000011);  // ADD A,
+  mcs48.writeROM(4, 128);         // #128
+  mcs48.writeROM(5, 0b00000011);  // ADD A,
+  mcs48.writeROM(6, 129);         // #129
+  mcs48.writeROM(7, 0b10100111);  // CPL C
+  mcs48.writeROM(8, 0b10010111);  // CLR C
+  mcs48.writeROM(9, 0b11010101);  // SEL RB1
+  mcs48.writeROM(10, 0b11000101); // SEL RB0
+  mcs48.writeROM(11, 0b00100011); // MOV A,
+  mcs48.writeROM(12, 255);        // #255
+  mcs48.writeROM(13, 0b11010011); // XRL A,
+  mcs48.writeROM(14, 0b10101010); // #0b10101010
 
-  for (const string &word : msg)
-  {
-    cout << word << " ";
-  }
-  cout << endl;
+  mcs48.reset();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  mcs48.clock();
+  mcs48.debug();
+
+  return 0;
 }
