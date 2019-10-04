4f81a6374bd3ab4ee40c804df773158e17fb7400
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-10-04 19:50:02 +0100

    Add header for Intel 8245 class

---
+++ src/intel8245.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <bitset>
+
+#include "bus.h"
+
+using namespace std;
+
+class INTEL8245
+{
+public:
+  INTEL8245();
+  ~INTEL8245();
+};
